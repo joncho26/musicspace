b4af4c80365bb195eeb82ff0c7354ba28339b256
Jonathan Cho <joncho26@gmail.com>
2015-02-18 13:31:28 -0500

    setting up ajax

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,20 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+  // 'get' ajax
+  $('.upvote-arrow').on("click",function(event){
+    event.preventDefault();
+    console.log('works');
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+    var id = $(event.target).data("artist");
+
+    $.ajax({
+      type:"GET",
+      url: "/upvote/" + id,
+      success: function(){
+
+      },
+      error: function(){
+
+      }
+    })
+  })
 });
