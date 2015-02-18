4ba0502b808e09444fc7c6352a7809283c6114e2
Jonathan Cho <joncho26@gmail.com>
2015-02-18 14:44:26 -0500

    add ajax call for upvotes

--- app/controllers/upvote_controller.rb
+++ app/controllers/upvote_controller.rb
@@ -1,5 +1,6 @@
 get '/upvote/:id' do
   artist = Artist.find(params[:id])
   Upvote.create(artist:artist, user:current_user)
-
+  # {upvote:artist.upvotes.length}.to_json
+  {upvote:artist.upvotes.length}.to_json
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,20 +1,20 @@
 $(document).ready(function() {
   // 'get' ajax
-  $('.upvote-arrow').on("click",function(event){
+  $('.artist-upvote-arrow').on("click",function(event){
     event.preventDefault();
-    console.log('works');
-
-    var id = $(event.target).data("artist");
+    var id = $(event.target).data("artist")
 
     $.ajax({
       type:"GET",
       url: "/upvote/" + id,
-      success: function(){
-
+      dataType: "JSON",
+      success:function(response){
+        $('.upvote-num').text(response.upvote)
       },
-      error: function(){
-
+      error:function(response){
+        console.log(response);
       }
     })
+
   })
 });
