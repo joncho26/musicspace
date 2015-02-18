94f470d72c7ca0ccaf4f2440d0c6c0e7e80e0107
Jonathan Cho <joncho26@gmail.com>
2015-02-18 09:39:46 -0500

    add user model

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ActiveRecord::Base
+  has_secure_password
+  has_many :upvotes
+  has_many :comments
+  has_many :followings
+  has_many :followed_artists, through: :followings, source: :artist
+end
