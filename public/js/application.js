43fdecbfd38a479c939d5621fdc2b036c9bb80ab
Jonathan Cho <joncho26@gmail.com>
2015-02-18 15:11:40 -0500

    add ajax for adding a new comment

--- public/js/application.js
+++ public/js/application.js
@@ -17,4 +17,26 @@ $(document).ready(function() {
     })
 
   })
+
+
+
+
+// ajax for adding comment
+ $('.comment-box').on("submit",function(event){
+  event.preventDefault();
+  // console.log('test');
+
+  var $target = $(event.target);
+  $.ajax({
+    url:$target.attr("action"),
+    type:$target.attr("method"),
+    data:$target.serialize(),
+    success:function(response){
+      $('ol.comment-area').prepend(response);
+    },
+    error:function(response){
+      console.log(response);
+    }
+  })
+ })
 });
