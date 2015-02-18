d7a74bd4792325563b075ba99c9e82aa3dc78655
Jonathan Cho <joncho26@gmail.com>
2015-02-18 10:19:12 -0500

    added login

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -6,6 +6,17 @@ get '/login' do
   erb :'auth/login'
 end
 
+post '/login' do
+  user = User.find_by(name: params[:user][:name])
+
+  if user.try(:authenticate, params[:user][:password])
+    session[:user_id] = user.id
+    redirect "/"
+  else
+    redirect "/login"
+  end
+end
+
 get '/signup' do
 
 end
