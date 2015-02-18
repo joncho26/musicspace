c0278eb4d64bb43f52bb674c2ac371faa7db8349
Jonathan Cho <joncho26@gmail.com>
2015-02-18 11:27:23 -0500

    add create new artist feature

--- app/controllers/artist_controller.rb
+++ app/controllers/artist_controller.rb
@@ -3,10 +3,21 @@ get '/artist/all' do
   erb :'artist/all'
 end
 
+get '/artist/new' do
+  erb :'artist/new'
+end
+
 get '/artist/:id' do |id|
   @artist = Artist.find(id)
   erb :'artist/artist_profile'
 end
+
+post '/artist/new' do
+  artist = Artist.create(params[:artist])
+  redirect "/artist/#{artist.id}"
+end
+
+
 
 
 
