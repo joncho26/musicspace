e86bfc8b65a3242cd4910778231962174bb8ccca
Jonathan Cho <joncho26@gmail.com>
2015-02-18 10:52:38 -0500

    redirects to profile page after login

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -11,7 +11,7 @@ post '/login' do
 
   if user.try(:authenticate, params[:user][:password])
     session[:user_id] = user.id
-    redirect "/"
+    redirect "/user/#{user.id}"
   else
     session[:error] ="Invalid Username or Password."
     redirect "/login"
