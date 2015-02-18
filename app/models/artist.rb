d2d29d9f1864c789405fccf01fa6066921021a76
Jonathan Cho <joncho26@gmail.com>
2015-02-18 13:15:35 -0500

    add helper method for artist that counts upvotes

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,4 +3,8 @@ class Artist < ActiveRecord::Base
   has_many :comments
   has_many :followings
   has_many :followed_users, through: :followings, source: :user
+
+  def total_votes
+    self.upvotes.length
+  end
 end
