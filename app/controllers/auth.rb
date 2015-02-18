f5e940bd6ffedd09789844e4040ba952ad9d9131
Jonathan Cho <joncho26@gmail.com>
2015-02-18 10:24:08 -0500

    added signup

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -18,5 +18,16 @@ post '/login' do
 end
 
 get '/signup' do
+  erb :'auth/signup'
+end
 
+post '/signup' do
+  user = User.create(params[:user])
+
+  if user.save
+    session[:user_id] = user.id
+    redirect "/"
+  else
+    redirect "/signup"
+  end
 end
